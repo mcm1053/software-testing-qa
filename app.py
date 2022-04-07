5cfc0e822c45f7a05b722fa38aafe81be734798e
Connor Moody <mcm1053@protonmail.com>
2022-04-07 12:25:10 -0500

    update

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-#!python3
+# A simple script to calculate BMI
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
 
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
+def bmi():
+    height = input("Input your height(in)：", type=FLOAT)
+    weight = input("Input your weight(lbs)：", type=FLOAT)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    bmi = ''
-    if request.method == 'POST' and 'weight' in request.form:
-        weight = float(request.form.get('weight'))
-        height = float(request.form.get('height'))
-        bmi = calc_bmi(weight, height)
-    return render_template("bmi_calc.html",
-	                        bmi=bmi)
+    # BMI = weight / (height / 100) ** 2
+    BMI = round(((weight * 703) / (height * height)), 2)
 
-def calc_bmi(weight, height):
-    #  return round((weight / ((height / 100) ** 2)), 2)
-    return round (((weight * 703) / (height * height)), 2)
+    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
+                  (25, 'Normal'), (30, 'Overweight'),
+                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
+
+    for top, status in top_status:
+        if BMI <= top:
+            put_text('Your BMI: %.1f. Category: %s' % (BMI, status))
+            break
+
 
 if __name__ == '__main__':
-    app.run()
+    bmi()
